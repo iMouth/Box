f9952e6770e21c229bd22084b627078276f660bd
Kelvin Kast <kelvin.8.kast@gmail.com>
2024-10-09 00:03:30 -0500

    Update folder names

--- box_creation.py
+++ box_creation.py
@@ -3,11 +3,11 @@ import box_sdk_gen as box
 
 with open("players.yaml", "r") as file:
     PLAYERS = yaml.load(file, Loader=yaml.FullLoader)
-START_DATE = "Sep 29"
-END_DATE = "Oct 4"
-CHALLENGE_DATE = "Sep 29"
-TRIBAL_DATE = "Oct 4"
-WEEK_NUMBER = 4
+START_DATE = "Oct 3"
+END_DATE = "Oct 10"
+CHALLENGE_DATE = "Oct 3"
+TRIBAL_DATE = "Oct 10"
+WEEK_NUMBER = 5
 FOLDER_ID = "284575669307" # Folder_ID is the ID of the root folder for a Survivor Texas season located in the URL 
 
 
@@ -37,7 +37,6 @@ def create_player_folders(client, parent_id):
         player_folder = create_folder(client, parent_id, f"{player['name']} Week {WEEK_NUMBER} ({START_DATE} - {END_DATE})")
         create_collaboration(client, player_folder.id, f"{player['eid']}@eid.utexas.edu")
         create_collaboration(client, player_folder.id, f"{player['eid']}@my.utexas.edu")
-        # If the week starts when the challenge starts then there was a double shoot so we don't need a pre-challenge folder.
         if START_DATE != CHALLENGE_DATE:
             create_folder(client, player_folder, f"Week {WEEK_NUMBER} Pre-Challenge ({START_DATE} - {CHALLENGE_DATE})")
         create_folder(client, player_folder, f"Week {WEEK_NUMBER} Pre-Tribal ({CHALLENGE_DATE} - {TRIBAL_DATE})")
@@ -49,9 +48,7 @@ def create_challenge_folders(client, parent_id):
     create_folder(client, challenge_folder, f"Week {WEEK_NUMBER} Challenge Videos")
     create_folder(client, challenge_folder, f"Week {WEEK_NUMBER} Mat Chat")
     confessional_folder = create_folder(client, challenge_folder, f"Week {WEEK_NUMBER} Confessionals")
-    # If the week starts when the challenge starts then there was a double shoot so we don't need a pre-challenge folder.
-    if START_DATE != CHALLENGE_DATE:
-        create_folder(client, confessional_folder, f"Week {WEEK_NUMBER} Pre-Challenge")
+    create_folder(client, confessional_folder, f"Week {WEEK_NUMBER} Pre-Challenge")
     create_folder(client, confessional_folder, f"Week {WEEK_NUMBER} Post-Challenge")
     create_folder(client, challenge_folder, f"Week {WEEK_NUMBER} Photographs")
     create_folder(client, challenge_folder, f"Week {WEEK_NUMBER} Miscellaneous")
@@ -59,9 +56,9 @@ def create_challenge_folders(client, parent_id):
 
 def create_tribal_folders(client, parent_id):
     """Create tribal-related folders."""
-    tribal_folder = create_folder(client, parent_id, f"Tribal ({TRIBAL_DATE})")
+    tribal_folder = create_folder(client, parent_id, f"Week {WEEK_NUMBER} Tribal ({TRIBAL_DATE})")
     create_folder(client, tribal_folder, f"Week {WEEK_NUMBER} Tribal Videos")
-    tribal_confessionals = create_folder(client, tribal_folder, "Confessionals")
+    tribal_confessionals = create_folder(client, tribal_folder, f" Week {WEEK_NUMBER} Confessionals")
     create_folder(client, tribal_confessionals, f"Week {WEEK_NUMBER} Pre-Tribal")
     create_folder(client, tribal_confessionals, f"Week {WEEK_NUMBER} Post-Tribal")
     create_folder(client, tribal_folder, f"Week {WEEK_NUMBER} Votes")
@@ -77,12 +74,12 @@ def main():
 
 
     root_folder = create_folder(client, box.CreateFolderParent(id=FOLDER_ID), f"Week {WEEK_NUMBER} ({START_DATE} - {END_DATE})")
-    player_self_uploads = create_folder(client, root_folder, "Player Self Uploads")
+    player_self_uploads = create_folder(client, root_folder, f"Week {WEEK_NUMBER} Player Self-Uploads")
     create_player_folders(client, player_self_uploads)
     create_challenge_folders(client, root_folder)
     create_tribal_folders(client, root_folder)
-    create_folder(client, root_folder, "Dream Team")
-    create_folder(client, root_folder, "Misc")
+    create_folder(client, root_folder, f"Week {WEEK_NUMBER} Dream Team")
+    create_folder(client, root_folder, f"Week {WEEK_NUMBER} Miscellaneous")
 
 
 if __name__ == '__main__':
